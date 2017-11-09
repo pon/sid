823130ee002a209b409aab38b3febde124075636
Peter Nagel <peter@meetearnest.com>
2017-11-08 21:36:39 -0800

    fix some fin styling

--- inside/src/components/Verification/FinancialSection.jsx
+++ inside/src/components/Verification/FinancialSection.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 import FinancialCredentialCard from './FinancialCredentialCard';
 
 const FinancialSection = ({financialCredentials, style}) => {
+  const cardStyle = {
+    width: '30%',
+    margin: '1%',
+    float: 'left'
+  };
+  
   return (
     <div style={style}>
       <h3>Connected Financial Accounts</h3>
       <div>
         {financialCredentials.map(credential => {
-          return (<FinancialCredentialCard key={credential.id} style={{width: '30%'}} financialCredential={credential} />);
+          return (<FinancialCredentialCard key={credential.id} style={cardStyle} financialCredential={credential} />);
         })}
       </div>
     </div>
