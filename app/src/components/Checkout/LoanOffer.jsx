27ce4390852f1ef3bd4a1f1be9276ebf59571040
Peter Nagel <peter@meetearnest.com>
2017-11-14 14:56:52 -0800

    loan offer page

--- app/src/components/Checkout/LoanOffer.jsx
+++ app/src/components/Checkout/LoanOffer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import Paper from 'material-ui/Paper';
+import Divider from 'material-ui/Divider';
+import {green50, green100} from 'material-ui/styles/colors';
 
 import RaisedButton from 'material-ui/RaisedButton';
 
 const LoanOfferWrapper = styled.div`
-  width: 50%;
+  width: 75%;
   margin: 0 auto;
   text-align: center;
 `;
@@ -12,11 +15,27 @@ const LoanOfferWrapper = styled.div`
 const LoanOffer = ({loanOffer, nextStep}) => {
   return (
     <LoanOfferWrapper>
-      <h2>Your Offer</h2>
-      <h4>${loanOffer && loanOffer.principal_amount} at {loanOffer && loanOffer.interest_rate / 100}% over {loanOffer && loanOffer.term_in_months} Months</h4>
-      <h5>Expires @ {loanOffer && loanOffer.expires_at}</h5>
+      <Paper style={{
+        paddingTop: '10px', 
+        fontSize: '16px', 
+        backgroundColor: green50, 
+        height: '500px',
+        borderStyle: 'solid',
+        borderWidth: '1.5px',
+        borderColor: green100
+      }}>
+        <p style={{fontSize: '24px'}}>Congratulations!</p>
+        <Divider style={{backgroundColor: green100}}/>
+        <p style={{paddingTop: '10px', textAlign: 'left', paddingRight: '10px', paddingLeft: '10px'}}>
+          <h4>Fees: $0</h4>
+          <h4>{`Monthly Payment: $${loanOffer.estimated_monthly_payment}`}</h4>
+          <h4>{`Number of Payments: ${loanOffer.term_in_months}`}</h4>
+          <h4>{`Total Loan Amount: $${loanOffer.principal_amount}`}</h4>
+          <h4>{`APR: ${loanOffer.interest_rate/100}%`}</h4>
+        </p>
+      </Paper>
       <br />
-      <RaisedButton primary={true} onClick={() => {return nextStep({loanOfferId: loanOffer.id})}}>Continue</RaisedButton>
+      <RaisedButton style={{width: '80%'}} primary={true} onClick={() => {return nextStep({loanOfferId: loanOffer.id})}}>Continue</RaisedButton>
     </LoanOfferWrapper>
   );
 }
--- app/src/components/common/Faq.jsx
+++ app/src/components/common/Faq.jsx
@@ -4,7 +4,7 @@ import Divider from 'material-ui/Divider';
 import {blue50} from 'material-ui/styles/colors';
 
 export default ({header, body}) => (
-  <Paper style={{padding: '10px', fontSize: '12px', backgroundColor: blue50}}>
+  <Paper style={{padding: '10px', fontSize: '14px', backgroundColor: blue50}}>
     <h6 style={{paddingRight: '10px', paddingLeft: '10px'}}>{header}</h6>
     <Divider />
     <p style={{paddingRight: '10px', paddingLeft: '10px', paddingTop: '10px'}}>
